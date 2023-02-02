8a9db3991d071d29fce518425f6ec44538435f58
AngelMerlinO <angelortegamerlin@gmail.com>
2023-02-01 22:59:29 -0600

    [fix] formulario registro

--- src/components/atoms/FormBus.jsx
+++ src/components/atoms/FormBus.jsx
@@ -2,31 +2,28 @@ import { useRef } from "react";
 import "../../assets/styles/FormBus.css";
 function FormBus() {
   const form = useRef();
-
   const handlerClick =(e) =>{
     e.preventDefault();
     const formData = new FormData(form.current);
     console.log(String(Math.floor(Math.random()*(9999 - 1000)+1000)));
     
-    let uri = 'http://34.255.239.102/api/autobus/register';
+    let uri = 'http://34.225.239.102/api/autobus/register';
     let options ={
-      method:'POST',
-      headers:{
-        "Content-Type": 'application/json'
-      },
-      body: JSON.stringify({
+        method:'POST',
+                  headers:{ "Content-Type":'application/json'},
+                    body:JSON.stringify({
         clave: formData.get('clave'),
         placa: formData.get('placa'),
-        numasientos: formData.get('asientos'),
+        numasientos: formData.get('numasientos'),
         fecha: formData.get('fecha'),
         tipo: formData.get('tipo'),
-        nombre: formData.get('chofer'),
+        nombre: formData.get('nombre'),
         licencia: String(Math.floor(Math.random()*(9999 - 1000)+1000))
       })
     }
     fetch(uri,options)
     .then((response)=>response.json())
-    .then((data)=> {alert(JSON.stringify(data))});
+    .then((data)=> {alert(JSON.stringify(data))})
   }
 
   return (
@@ -40,7 +37,7 @@ function FormBus() {
         <input type="text" className="input-bus" id="placa" name="placa" />
 
         <label className="text-input" htmlFor="asientos">Numero de asientos</label>
-        <input type="number" className="input-bus" id="asientos" name="asientos"/>
+        <input type="number" className="input-bus" id="asientos" name="numasientos"/>
 
         <label className="text-input" htmlFor="fecha">Fecha de alta</label>
         <input type="date" className="input-bus" id="fecha" name="fecha" />
@@ -52,7 +49,7 @@ function FormBus() {
         </select>
 
         <label className="text-input" htmlFor="chofer">Nombre del chofer</label>
-        <input type="text" className="input-bus"  id="chofer" name="chofer"/>
+        <input type="text" className="input-bus"  id="chofer" name="nombre"/>
 
         <label className="text-input"> Numero de licencia </label>
         <input type="number" className="input-bus" />
